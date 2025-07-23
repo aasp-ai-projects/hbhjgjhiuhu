0c946f35c71647d00cb013903da1ab108d4e9619
Sandeep-Zedblock <151967898+Sandeep-Zedblock@users.noreply.github.com>
2025-07-23 21:33:03 +0530

    Add src/app/page.tsx from template

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+   <div className="flex items-center justify-center min-h-screen ">
+    <h1 className="text-yellow-700 text-6xl">Hello</h1>
+   </div>
+  );
+}
